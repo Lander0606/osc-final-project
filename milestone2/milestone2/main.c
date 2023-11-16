6862cfb424487dfd277a462ab90c0aa6be2290f7
Lander0606 <landervanloock@gmail.com>
2023-11-16 17:35:55 +0100

    added defensive programming and error reporting

--- milestone2/milestone2/main.c
+++ milestone2/milestone2/main.c
@@ -12,7 +12,9 @@
 
 int main()
 {
-    FILE *f = open_db("sensor_db.csv", true);
+    // First test: correct use of sensor_db functions
+    printf("***** TEST 1: *****\n");
+    FILE *f = open_db(NULL, true);
 
     sleep(1);
     sensor_id_t id = 1;
@@ -32,5 +34,27 @@ int main()
     insert_sensor(f, 4, v, ts);
 
     close_db(f);
+
+    // Second test: invalid database file name
+    printf("***** TEST 2: *****\n");
+    FILE *f1 = open_db(NULL, true);
+    insert_sensor(f1, id, v, ts);
+    close_db(f1);
+
+    // Third test: no file opened when inserting
+    printf("***** TEST 3: *****\n");
+    insert_sensor(f1, id, v, ts);
+
+    // Fourth test: close a non-opened file
+    printf("***** TEST 4: *****\n");
+    close_db(f1);
+
+    // Fifth test: opening a database twice and closing both
+    printf("***** TEST 5: *****\n");
+    FILE * f2 = open_db("sensor_db.csv", true);
+    FILE * f3 = open_db("sensor_db.csv", true);
+    close_db(f2);
+    close_db(f3);
+
     return 0;
 }
--- milestone2/milestone2/sensor_db.c
+++ milestone2/milestone2/sensor_db.c
@@ -9,26 +9,33 @@
 #define WRITE_END 1
 
 // Define the non-printable ASCII characters for insert and end message
-#define END_MSG 27
 #define INSERT_MSG 17
+#define OPEN_MSG 18
+#define CLOSE_MSG 19
+#define OPEN_ERR_MSG 20
+#define INSERT_ERR_MSG 21
+#define CLOSE_ERR_MSG 22
 
 int fd[2];
+bool child_created = false;
 
 int create_child();
 
 int create_child() {
 
     if (pipe(fd) == -1){
-        printf("Pipe creation error\n");
-        return 1;
+        printf("Error while creating pipe\n");
+        return -1;
     }
 
+    child_created = true;
     pid_t pid;
     pid = fork();
 
     if (pid < 0) { // Child process creation error
-        printf("Child process creation error\n");
-        return 1;
+        printf("Error while creating child process\n");
+        child_created = false;
+        return -1;
     }
 
     if (pid > 0) { // Parent process code
@@ -36,16 +43,28 @@ int create_child() {
     }
 
     else { // Child process code
+
         char read_msg;
         close(fd[WRITE_END]);
         create_log_process();
-        write_to_log_process("Data file opened.");
         read(fd[READ_END], &read_msg, SIZE);
-        while(read_msg != END_MSG) {
+
+        while(read_msg != CLOSE_MSG) {
+
             if(read_msg == INSERT_MSG)
                 write_to_log_process("Data inserted.");
+            else if(read_msg == INSERT_ERR_MSG)
+                write_to_log_process("Error while inserting data");
+            else if(read_msg == OPEN_MSG)
+                write_to_log_process("Data file opened.");
+            else if(read_msg == OPEN_ERR_MSG)
+                write_to_log_process("Error while opening file: database file already opened");
+            else if(read_msg == CLOSE_ERR_MSG)
+                write_to_log_process("Error while closing file.");
+
             read(fd[READ_END], &read_msg, SIZE);
         }
+
         write_to_log_process("Data file closed.");
         end_log_process();
         close(fd[READ_END]);
@@ -58,30 +77,88 @@ int create_child() {
 
 FILE * open_db(char * filename, bool append) {
 
-    create_child();
+    if(child_created) {
+        char write_msg = OPEN_ERR_MSG;
+        write(fd[WRITE_END], &write_msg, SIZE);
+        return NULL;
+    } else if (filename == NULL) {
+        printf("Error while opening file: invalid filename pointer\n");
+        return NULL;
+    }
+
+    FILE * fp;
 
     if(append)
-        return fopen(filename, "a");
+        fp = fopen(filename, "a");
     else
-        return fopen(filename, "w");
+        fp = fopen(filename, "w");
 
+    if(fp != NULL) {
+        int result = create_child();
+        if(result < 0) {
+            fclose(fp);
+            return NULL;
+        }
+        char write_msg = OPEN_MSG;
+        write(fd[WRITE_END], &write_msg, SIZE);
+    } else {
+        printf("Error while opening file: file open error\n");
+    }
+    return fp;
 }
 
 int insert_sensor(FILE * f, sensor_id_t id, sensor_value_t value, sensor_ts_t ts) {
 
-    char write_msg = INSERT_MSG;
-    write(fd[WRITE_END], &write_msg, SIZE);
+    if(f == NULL && child_created) {
+        char write_msg = INSERT_ERR_MSG;
+        write(fd[WRITE_END], &write_msg, SIZE);
+        return -1;
+    } else if(f == NULL) {
+        printf("Error while inserting: invalid file pointer\n");
+        printf("Error while inserting: database file not opened yet\n");
+        return -1;
+    } else if(!child_created) {
+        printf("Error while inserting: database file not opened yet\n");
+        return -1;
+    }
 
-    return fprintf(f, "%d, %f, %ld\n", id, value, ts);
+    int result = fprintf(f, "%d, %f, %ld\n", id, value, ts);
+    if(result > 0) {
+        char write_msg = INSERT_MSG;
+        write(fd[WRITE_END], &write_msg, SIZE);
+    } else {
+        char write_msg = INSERT_ERR_MSG;
+        write(fd[WRITE_END], &write_msg, SIZE);
+    }
+
+    return result;
 
 }
 
 int close_db(FILE * f) {
 
-    char write_msg = END_MSG;
-    write(fd[WRITE_END], &write_msg, SIZE);
-    close(fd[WRITE_END]);
+    if(f == NULL && child_created) {
+        char write_msg = CLOSE_ERR_MSG;
+        write(fd[WRITE_END], &write_msg, SIZE);
+        return -1;
+    } else if (f == NULL) {
+        printf("Error while closing file: invalid file pointer\n");
+        return -1;
+    }
 
-    return fclose(f);
+    int result = fclose(f);
+
+    if(result == 0) {
+        char write_msg = CLOSE_MSG;
+        write(fd[WRITE_END], &write_msg, SIZE);
+        close(fd[WRITE_END]);
+    } else if(child_created) {
+        char write_msg = CLOSE_ERR_MSG;
+        write(fd[WRITE_END], &write_msg, SIZE);
+    } else {
+        printf("Error while closing file: database file not opened yet\n");
+    }
+
+    return result;
 
 }
